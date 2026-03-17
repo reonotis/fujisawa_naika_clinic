741a494ae7aaf447d0acbbc6c6dde1fea48b839b
REONOTIS\fujisawa <fujisawa@reonotis.jp>
2026-03-17 23:08:15 +0900

    画像入れ替え

--- resources/views/layouts/partials/hero-slideshow.blade.php
+++ resources/views/layouts/partials/hero-slideshow.blade.php
@@ -3,7 +3,7 @@
 @endpush
 
 <div class="hero-media">
-    @foreach(['clinic_view.jpg', 'CT_scanner.jpg', 'endoscope.jpg', 'fundus-camera.jpg'] as $i => $filename)
+    @foreach(['clinic_view.jpg', 'CT_scanner.jpg', 'endoscope.jpg', 'reception_space.jpg'] as $i => $filename)
     <div class="hero-slide" role="img" aria-label="クリニックイメージ {{ $i + 1 }}">
         <img src="{{ asset('images/slideshow/' . $filename) }}" alt="クリニック外観・院内イメージ {{ $i + 1 }}" />
     </div>
