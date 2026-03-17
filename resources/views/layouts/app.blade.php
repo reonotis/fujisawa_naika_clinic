e362b321849a3ce1392b436578eb970ba4c99d9a
REONOTIS\fujisawa <fujisawa@reonotis.jp>
2026-03-18 00:22:34 +0900

    ロゴ修正

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
     <meta property="og:description" content="千葉県流山市の藤澤内科クリニック公式サイト。内科・循環器内科を中心に、生活習慣病（高血圧・糖尿病・脂質異常症）や風邪・発熱などの一般内科診療を行っています。診療内容、診療時間、アクセス、休診日やお知らせをわかりやすくご案内します。" />
     <meta property="og:type" content="website" />
     <meta property="og:url" content="{{ url()->current() }}" />
-    <meta property="og:image" content="{{ asset('images/logo.png') }}" />
+    <meta property="og:image" content="{{ asset('images/logo_2.png') }}" />
     <meta property="og:site_name" content="藤澤内科クリニック" />
     <link rel="icon" type="image/png" href="{{ asset('images/icon.png') }}" />
 
