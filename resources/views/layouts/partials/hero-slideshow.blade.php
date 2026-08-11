0860ec60da4e3256653cf67eef8efa7a29fd28e5
REONOTIS\fujisawa <fujisawa@reonotis.jp>
2026-08-12 01:22:05 +0900

    バナー作成

--- resources/views/layouts/partials/hero-slideshow.blade.php
+++ resources/views/layouts/partials/hero-slideshow.blade.php
@@ -5,7 +5,7 @@
 <div class="hero-media">
     @foreach(['clinic_view.jpg', 'CT_scanner.jpg', 'endoscope.jpg', 'reception_space.jpg'] as $i => $filename)
     <div class="hero-slide" role="img" aria-label="クリニックイメージ {{ $i + 1 }}">
-        <img src="{{ asset('images/slideshow/' . $filename) }}" alt="クリニック外観・院内イメージ {{ $i + 1 }}" />
+        <img src="{{ asset('images/slideshow/' . $filename . '?v=1') }}" alt="クリニック外観・院内イメージ {{ $i + 1 }}" />
     </div>
     @endforeach
 </div>
